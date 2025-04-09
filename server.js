47443078ac466f44aa82261155e6476846fa11c0
sujanr <sujanrumakantha@gmail.com>
2025-04-09 16:55:46 +0530

    added sachin

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors({
 }))
 
 app.get("/",(req,res)=>{
-    res.status(200).json({success:true,message:"Hello World samith"})
+    res.status(200).json({success:true,message:"Hello World sachin"})
 })
 
 app.listen(5000,()=>{
