4f227b23cc5f4cfb874ec660734bcf4f558bc885
sujanr <sujanrumakantha@gmail.com>
2025-04-09 16:53:24 +0530

    added samith

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors({
 }))
 
 app.get("/",(req,res)=>{
-    res.status(200).json({success:true,message:"Hello World 3"})
+    res.status(200).json({success:true,message:"Hello World samith"})
 })
 
 app.listen(5000,()=>{
